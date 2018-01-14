bc7fdc4751acde7ae56e93bcaacd5c0ce0abea63
RomanRanniew <romanranniew@gmail.com>
2018-01-14 19:32:44 +0100

    Init Inspector in vue

---
+++ Inspector/src/renderer/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+  <router-view/>
+	<router-link
+				 to="/"
+				 >Home</router-link>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: 'inspector'
+	}
+
+</script>
+
+<style src="./styles/global.less" lang="less"></style>
+
+<style lang="less">
+	@import (css) url("https://fonts.googleapis.com/icon?family=Roboto");
+	html,
+	body {
+		font-family: Roboto;
+		font-weight: 300;
+		background: #333844;
+		color: white;
+	}
+
+</style>
---
+++ Inspector/src/renderer/components/Journal.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="journal">
+        Journal {{filename}}
+        <router-link
+           to="/"
+         >Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'journal',
+        data: {
+            filename: 's'
+        }
+    }
+</script>
+
+<style scoped lang="less">
+</style>
